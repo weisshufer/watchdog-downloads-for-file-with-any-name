b331643b444b3e37df4131a5ce7117f2a63c27c3
JustGo1909 <alexeybelok@gmail.com>
2024-01-26 15:45:18 +0100

    fix: watcher for Windows

--- main.py
+++ main.py
@@ -4,15 +4,30 @@ import platform
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+target_file_name = "Abtretungserklärung_signiert_"
+target_file_extension = "pdf"
+
 
 class DownloadFolderHandler(FileSystemEventHandler):
+
     def on_created(self, event):
         # Получаем полное имя файла
         filename = os.path.basename(event.src_path)
 
         # Waiting for file with
-        if "Abtretungserklärung_signiert_" in filename:
+        if filename.startswith(target_file_name) and filename.endswith("pdf"):
             # Выполнение команды
+            print(f"Обнаружен файл: {filename}. Выполняется команда...")
+
+    def on_modified(self, event):
+        time.sleep(1)
+        # Получаем полное имя файла
+        filename = os.path.basename(event.src_path)
+        print(f"Измененный файл: {filename}")  # Для отладки
+
+        if filename.startswith(target_file_name) and not filename.endswith((".crdownload", ".tmp")) and filename.endswith(".pdf"):
+            print(f"Файл соответствует условиям: {filename}")  # Дополнительная отладка
+            # Если файл соответствует условиям, выполняем команду
             print(f"Обнаружен файл: {filename}. Выполняется команда...")
 
 
